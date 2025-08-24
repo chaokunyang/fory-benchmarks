f4df8a39d5633a16b88a87c1fd86e889905881d6
Shawn Yang <shawn.ck.yang@gmail.com>
2025-08-24 20:51:49 +0800

    add cbor benchmark (#8)

---
+++ src/main/java/com/github/chaokunyang/fury/benchmark/JacksonCborBenchmark.java
@@ -0,0 +1,60 @@
+package com.github.chaokunyang.fury.benchmark;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.chaokunyang.fury.benchmark.data.Image;
+import com.github.chaokunyang.fury.benchmark.data.MediaContent;
+import com.github.chaokunyang.fury.benchmark.data.Struct;
+import com.github.chaokunyang.fury.benchmark.data.Media;
+import org.apache.fury.Fury;
+import org.openjdk.jmh.Main;
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.io.IOException;
+
+public class JacksonCborBenchmark extends BenchmarkBase {
+  private static final ObjectMapper mapper = new CBORMapper(); // create once, reuse
+  private static byte[] jacksonMediaContentBytes;
+  private static byte[] jacksonStructBytes;
+  static {
+    try {
+      jacksonMediaContentBytes = mapper.writeValueAsBytes(mediaContent);
+      jacksonStructBytes = mapper.writeValueAsBytes(struct);
+      System.out.println("jacksonMediaContentBytes size " + jacksonMediaContentBytes.length);
+      System.out.println("jacksonStructBytes size " + jacksonStructBytes.length);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Benchmark
+  public Object jacksonSerializeMediaContent() throws Exception {
+    return mapper.writeValueAsBytes(mediaContent);
+  }
+
+  @Benchmark
+  public Object jacksonDeserializeMediaContent() throws Exception {
+    return mapper.readValue(jacksonMediaContentBytes, MediaContent.class);
+  }
+
+  @Benchmark
+  public Object jacksonSerializeStruct() throws Exception {
+    return mapper.writeValueAsBytes(struct);
+  }
+
+  @Benchmark
+  public Object jacksonDeserializeStruct() throws Exception {
+    return mapper.readValue(jacksonStructBytes, Struct.class);
+  }
+
+  public static void main(String[] args) throws IOException {
+    if (args.length == 0) {
+      String commandLine =
+        "com.*JacksonCborBenchmark.* -f 3 -wi 5 -i 5 -t 1 -w 2s -r 2s -rf csv";
+      System.out.println(commandLine);
+      args = commandLine.split(" ");
+    }
+    Main.main(args);
+  }
+}
